59f016503e0a4dc8357db267b022a9ae7d54cb94
NakulKachhava <24010101116@darshan.ac.in>
2025-08-07 13:44:33 +0530

    Update lab16A1.c

--- lab16A1.c
+++ lab16A1.c
@@ -7,43 +7,73 @@ struct node {
     struct node *rptr;
 };
 
-struct node* insert(struct node *root){
-    int x;
-    printf("Enter value of x - ");
-    scanf("%d",&x);
+struct node* insert(struct node *root, int key){
     if(root == NULL){
         struct node *newnode = (struct node *)malloc(sizeof(struct node));
-        newnode->info = x;
+        newnode->info = key;
         newnode->lptr = newnode->rptr = NULL;
         return newnode;
     }
-    if(x < root->info) {
-        root->lptr = insert(root->lptr);
-    } else if(x > root->info) {
-        root->rptr = insert(root->rptr);
+    if(key < root->info) {
+        root->lptr = insert(root->lptr, key);
+    } else if(key > root->info) {
+        root->rptr = insert(root->rptr, key);
     } else {
         printf("Duplicate value, not inserting.\n");
     }
     return root;
 }
 
-void search(struct node *root){
-    int x;
-    printf("Enter value you want to search - ");
-    scanf("%d",&x);
-    if(root == NULL){
+void search(struct node *root, int x) {
+    if (root == NULL) {
+        printf("%d is not present in the tree\n", x);
         return;
-    }
-    else if(root->info == x){
-        printf("%d is present in the tree",x);
+    } else if (root->info == x) {
+        printf("%d is present in the tree\n", x);
         return;
+    } else if (x < root->info) {
+        search(root->lptr, x);
+    } else {
+        search(root->rptr, x);
     }
-    else{
-        printf("%d is not present in the tree",x);
-        return;
+}
+
+struct node* minValueNode(struct node* node) {
+    struct node* temp = node;
+    
+    while (temp && temp->lptr != NULL)
+        temp = temp->lptr;
+    
+    return temp;
+}
+
+
+struct node* delete(struct node* root, int key) {
+    if (root == NULL) return root;
+    
+    if (key < root->info)
+        root->lptr = delete(root->lptr, key);
+    else if (key > root->info)
+        root->rptr = delete(root->rptr, key);
+    else {
+        if (root->lptr == NULL) {
+            struct node* temp = root->rptr;
+            free(root);
+            return temp;    
+        }
+        else if (root->rptr == NULL) {
+            struct node* temp = root->lptr;
+            free(root);
+            return temp;
+        }
+        
+        struct node* temp = minValueNode(root->rptr);  
+        
+        root->info = temp->info;
+        
+        root->rptr = delete(root->rptr, temp->info);
     }
-    search(root->lptr);
-    search(root->rptr);
+    return root;
 }
 
 void preOrderTraversal(struct node *root){
@@ -59,8 +89,8 @@ void postOrderTraversal(struct node *root){
     if(root == NULL){
         return;
     }
-    preOrderTraversal(root->lptr);
-    preOrderTraversal(root->rptr);
+    postOrderTraversal(root->lptr);
+    postOrderTraversal(root->rptr);
     printf("%d,",root->info);
 }
 
@@ -68,27 +98,42 @@ void inOrderTraversal(struct node *root){
     if(root == NULL){
         return;
     }
-    preOrderTraversal(root->lptr);
+    inOrderTraversal(root->lptr);
     printf("%d,",root->info);
-    preOrderTraversal(root->rptr);
+    inOrderTraversal(root->rptr);
 }
 
 int main(){
-    struct node *root;
+    struct node *root = NULL;
     int choice;
     while (1)
     {
-        printf("Menu: \n");
+        printf("\nMenu: \n");
         printf("1.Insert a node in the tree. \n2.Delete a node from the tree.\n3.Search a node in the tree.\n4.Perform Preorder Traversal in the tree.\n5.Perform Postorder Traversal in the tree.\n6.Perform Inorder Traversal in the tree.\n7.Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
         switch (choice)
         {
-        case 1: root = insert(root);
+        case 1: {
+            int key;
+            printf("Enter node you want to insert - ");
+            scanf("%d",&key);
+            root = insert(root,key);
+        }
         break;
-        // case 2: delete(root);
-        // break;
-        case 3: search(root);
+        case 2: {
+            int key;
+            printf("Enter node you want to delete - ");
+            scanf("%d",&key);
+            root = delete(root,key);
+        }
+        break;
+        case 3: {
+            int key;
+            printf("Enter value you want to search: ");
+            scanf("%d", &key);
+            search(root, key);
+        }
         break;
         case 4: preOrderTraversal(root);
         break;
